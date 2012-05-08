6ceee6ff13f4966ea93b002a4fde4db88e9a1ee3
Andre Schloemp <andre.schloemp@me.com>
2012-05-08 21:53:48 +0200

    x

---
+++ src/com/mahn42/framework/Framework.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mahn42.framework;
+
+/**
+ *
+ * @author andre
+ */
+public class Framework {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
