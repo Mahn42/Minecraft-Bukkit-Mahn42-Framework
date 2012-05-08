1af1a21ba7ca66e5642c937a2778ad231f2a31e2
Andre Schloemp <andre.schloemp@me.com>
2012-05-08 22:04:36 +0200

    x

--- src/com/mahn42/framework/Framework.java
+++ src/com/mahn42/framework/Framework.java
@@ -4,16 +4,25 @@
  */
 package com.mahn42.framework;
 
+import org.bukkit.plugin.java.JavaPlugin;
+
 /**
  *
  * @author andre
  */
-public class Framework {
+public class Framework extends JavaPlugin {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+    }
+
+    @Override
+    public void onEnable() { 
+    }
+
+    @Override
+    public void onDisable() { 
     }
 }
