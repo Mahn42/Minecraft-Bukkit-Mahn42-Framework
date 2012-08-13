7975b90d5e8ffc8270a2e1d10350e1046b50a94e
Andre Schloemp <andre.schloemp@me.com>
2012-08-14 00:19:57 +0200

    - removed EMERALD on PlayerListener

--- src/com/mahn42/framework/PlayerListener.java
+++ src/com/mahn42/framework/PlayerListener.java
@@ -80,11 +80,13 @@ public class PlayerListener implements Listener {
             lTask.position = new BlockPosition(lBlock.getLocation());
             Framework.plugin.getServer().getScheduler().scheduleAsyncDelayedTask(Framework.plugin, lTask);
         }
+        /*
         if (lBlock != null
                 && aEvent.getAction().equals(Action.RIGHT_CLICK_BLOCK)
                 && aEvent.hasItem()
                 && lInHand.equals(Material.EMERALD)) {
             //TODO protect building
         }
+        */
     }
 }
